8e8aee2d76242f29900dbc5e9cc67714988d5179
Adem Bekteshi <adem.bekteshi@infis.cz>
2026-05-26 09:35:15 +0200

    cc

---
+++ WpfApp4/GameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    public class GameState
+    {
+        public double Cookies { get; set; }
+        public double CookiesPerClick { get; set; }
+        public double CookiesPerSecond { get; set; }
+        public int TotalClicks { get; set; }
+        public int TotalUpgradesBought { get; set; }
+    }
+}
